32a375674633aeb7a6165c9d7f657336281db998
Gabriela Vicentini <gabrivicentini@icloud.com>
2025-05-12 19:25:22 -0300

    Update App.tsx
    to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MatchInfo from './Components/MatchInfo/MatchInfo';
 import './styles.css';
 
 function App() {
+export const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3001";
 
   return (
     <div className='mainPage'>
