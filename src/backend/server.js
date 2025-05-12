5faac5a62efa1af22f462bf2f57fa25c5ed52fc4
Gabriela Vicentini <gabrivicentini@icloud.com>
2025-05-12 19:21:52 -0300

    Update server.js
    to deploy

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,38 +1,40 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import { callOpenRouter, getLatestMatches } from '../utils/backendUtils.js';
+import path from "path";
+import express from "express";
+import cors from "cors";
+import { callOpenRouter, getLatestMatches } from "../utils/backendUtils.js";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.use(json());
+app.use(express.static(path.join(process.cwd(), "src", "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "src", "dist", "index.html"));
+});
 
 app.listen(3001, () => {
-  console.log('Servidor rodando na porta 3001');
+  console.log("Servidor rodando na porta 3001");
 });
 
-app.get('/matches', async (_req, res) => {
+app.get("/matches", async (_req, res) => {
   try {
     const latestMatches = await getLatestMatches();
-    
     res.json(latestMatches);
   } catch (error) {
-    console.error('Erro ao buscar as partidas:', error);
-    res.status(500).json({ success: false, error: 'Erro ao buscar as partidas' });
+    console.error("Erro ao buscar as partidas:", error);
+    res.status(500).json({ success: false, error: "Erro ao buscar as partidas" });
   }
 });
 
-app.post('/send-message', async (req, res) => {
+app.post("/send-message", async (req, res) => {
   const { message } = req.body;
-
   try {
     const reply = await callOpenRouter(message);
-
     res.json({ success: true, reply });
   } catch (error) {
-    console.error('Erro ao processar a mensagem:', error);
-    res.status(500).json({ success: false, error: 'Erro ao processar a mensagem' });
+    console.error("Erro ao processar a mensagem:", error);
+    res.status(500).json({ success: false, error: "Erro ao processar a mensagem" });
   }
 });
-
